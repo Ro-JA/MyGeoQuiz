0687b1015439428af8d87543da17c9a5f528b00d
Roman <boyra75.rb@gmail.com>
2021-12-26 19:45:02 +0900

    working option

--- app/src/main/java/com/bignerdranch/android/mygeoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/mygeoquiz/CheatActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
 import com.bignerdranch.android.mygeoquiz.R
@@ -27,31 +28,44 @@ class CheatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
 
-        wasCheated = savedInstanceState?.getBoolean(KEY_WAS_CHEATED, false)
+        wasCheated = savedInstanceState?.getBoolean(KEY_WAS_CHEATED, false) ?: false
         setAnswerShownResult(wasCheated)
+
 
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
         answerTextView = findViewById(R.id.answer_text_view)
         showAnswerButton = findViewById(R.id.show_answer_button)
         showAnswerButton.setOnClickListener {
-            val answerText = when {
-                wasCheated = true
-                setAnswerShownResult(true)
-                fillTextIfCheated()
-//                answerIsTrue -> R.string.true_button
-//                else -> R.string.false_button
-            }
-//            answerTextView.setText(answerText)
-//            setAnswerShownResult(true)
+            wasCheated = true
+            setAnswerShownResult(true)
             fillTextIfCheated()
+        }
+
+        fillTextIfCheated()
+    }
+
+    private fun fillTextIfCheated() {
+        if (wasCheated) {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY_WAS_CHEATED, wasCheated)
+    }
+
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
-        val data = Intent().apply {
-            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        if (wasCheated) {
+            val data = Intent().apply {
+                putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+            }
+            setResult(Activity.RESULT_OK, data)
         }
-        setResult(Activity.RESULT_OK, data)
     }
 
     companion object {
